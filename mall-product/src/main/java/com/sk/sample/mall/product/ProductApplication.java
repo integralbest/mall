0611229b5a90d8aa9cc0912c7035bac1ca2d0125
devopsedu01 <devopsedu01@gmail.com>
2019-05-09 08:48:19 +0900

    initial

--- mall-product/src/main/java/com/sk/sample/mall/product/ProductApplication.java
+++ mall-product/src/main/java/com/sk/sample/mall/product/ProductApplication.java
@@ -1,26 +1,12 @@
 package com.sk.sample.mall.product;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.sk.sample.mall.product.domain.model.ColorType;
-import com.sk.sample.mall.product.domain.model.Money;
-import com.sk.sample.mall.product.domain.model.Product;
-import com.sk.sample.mall.product.domain.model.ProductDescription;
-import com.sk.sample.mall.product.domain.model.SizeType;
-import com.sk.sample.mall.product.domain.repository.ProductRepository;
 
 @SpringBootApplication
 public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner createSampleData(ProductRepository productRepository) {	
-		return (args) -> {
 	}
 }
